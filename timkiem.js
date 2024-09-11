86d89ed05c708b9340af65f65236458730c5470f
VanDai <140904932+0919514675@users.noreply.github.com>
2024-09-11 14:59:53 +0700

    Add files via upload

---
+++ timkiem.js
@@ -0,0 +1,18 @@
+function timKiemSanPham(inp) {
+    var kieuTim = document.getElementsByName('kieuTimSanPham')[0].value;
+    var text = inp.value;
+
+    // Lọc
+    var vitriKieuTim = {'ma':1, 'ten':2}; // mảng lưu vị trí cột
+
+    var listTr_table = document.getElementsByClassName('sanpham')[0].getElementsByClassName('table-content')[0].getElementsByTagName('tr');
+    for (var tr of listTr_table) {
+        var td = tr.getElementsByTagName('td')[vitriKieuTim[kieuTim]].innerHTML.toLowerCase();
+
+        if (td.indexOf(text.toLowerCase()) < 0) {
+            tr.style.display = 'none';
+        } else {
+            tr.style.display = '';
+        }
+    }
+}
