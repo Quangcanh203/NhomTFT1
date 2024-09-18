4c144f22e879a0ba7ae03800f2d63c26523384c6
SeaLeeDawng1one <140811623+SeaLeeDawng1one@users.noreply.github.com>
2024-09-18 14:16:21 +0700

    đơn hàng/ OrtherDetail.cs

---
+++ đơn hàng/OrtherDetail.cs
@@ -0,0 +1,18 @@
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    namespace DoAn.Models
+    {
+        public class OtherDetail
+        {
+            public int Id { get; set; }
+            public string UserName { get; set; }
+            public string OrderCode { get; set; }
+            public long ProductId { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+
+            // Navigation property
+            public ProductModel Product { get; set; }
+        }
+    }
